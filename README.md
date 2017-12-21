b24cd52033789ab8b18b2dac6b09da2d2c74692d
JJd Cummings <jjcummin@cisco.comm>
2017-12-21 11:50:18 -0700

    First commit, yeah buddy

---
+++ README.md
@@ -0,0 +1,13 @@
+# MD2017 Repo
+
+I'll put things in here that I create or find useful for the MD2017.
+
+Presently there is only one file in here, a simple python script that creates formatted csv for use in importing into the MD2017.  Unfortunately because of lazy developers I had to create this.  
+
+Usage is simple...
+
+1. Clone it
+2. Run it and redirect the output 
+
+Example:
+`./gencontacts.py >contacts.csv`
---
+++ gencontacts.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+	# Simple script to generate a csv containing DMR contacts for the MD2017
+	# Note that the generated formatting (with extra spaces etc) will
+	# look funny, this is done becuase the MD2017 doesn't actually format
+	# the data correctly, so as a quick fix I'm using the screenwrap width 
+	# of the MD2017 to format it correctly... TYT developers are a joke
+
+    # Copyright (C) 2017  J Cummings (N0PKT)
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+	
+	# As a side note, it looks like it's actually doing some calculations with
+	# numeric values in this third field... could make for some interesting fun 
+	# with buffers....
+
+import requests
+
+def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
+
+def main():
+	dmrData = requests.get('http://www.dmr-marc.net/cgi-bin/trbo-database/datadump.cgi?table=users&format=csv&header=0') 
+	dmrData.encoding = dmrData.apparent_encoding
+	contacts = dmrData.iter_lines()
+	for record in contacts:
+		record = removeNonAscii(record)
+		contact = record.split(",")
+
+		p = 25 # looks like they are using 25 chars for the first "name" location and 24 thereafter
+		
+		# handle the extra-long names
+		if len(contact[2]) > 25:
+			while len(contact[2]) < 49:
+				contact[2] += " "
+			p = 24
+
+		# process all entries and add whitespace as-needed
+		i = 2
+		while i < len(contact):
+			while len(contact[i]) < p:
+				contact[i] += " "
+			i += 1
+			p = 24
+
+		# all set, output to stdout
+		print("{},{},{}{}{}{}").format(contact[0],contact[1],contact[2],contact[3],contact[4],contact[5].strip())
+
+if __name__ == "__main__":
+	main()
