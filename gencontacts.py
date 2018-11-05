1f6922e773ae271cb63c3b95f1cd74bf4daf19bd
JJd Cummings <jjcummin@cisco.comm>
2018-11-05 15:11:56 -0700

    updated to allow for countries and new radioid location

--- gencontacts.py
+++ gencontacts.py
@@ -27,16 +27,37 @@
 
 import requests
 
+# The following variable is a list of countries to include in the output because of
+# the need to limit the size of the contact list for the MD2017..
+# Country lists fund here: https://www.dmr-marc.net/FAQ/GSM-ID.html
+prefixes = [31,11,23,22,26,50]
+
 # because there are some nasty encoded chars in this data, let's lose em
 def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
 
+# becuase some radios are limited to a set number of contact records (like 100,000 on the MD2017) it is 
+# necisarry to limit the number of records.. therefore this function exists to filter (inclusively) which
+# records from where..
+def includeGeo(record):
+	try:
+		for prefix in prefixes:
+			if record.startswith(str(prefix)):
+				return 1
+		# If we have exhausted our match list then lets return zarro	
+		return 0
+	except:
+		return 1
+
 def main():
-	dmrData = requests.get('http://www.dmr-marc.net/cgi-bin/trbo-database/datadump.cgi?table=users&format=csv&header=0') 
+	dmrData = requests.get('https://www.radioid.net/static/users.csv') 
 	dmrData.encoding = dmrData.apparent_encoding
 	contacts = dmrData.iter_lines()
 	for record in contacts:
 		record = removeNonAscii(record)
 		contact = record.split(",")
+
+		if includeGeo(contact[0]) == 0:
+			continue
 
 		p = 25 # looks like they are using 25 chars for the first "name" location and 24 thereafter
 		
